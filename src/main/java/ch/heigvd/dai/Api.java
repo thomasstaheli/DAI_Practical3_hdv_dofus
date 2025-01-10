707e27a726e0cb1a83d1ca77c7431736bb7aba8f
Léonard Jouve <leonard.jouve@gmail.com>
2025-01-10 08:58:03 +0100

    Api class

---
+++ src/main/java/ch/heigvd/dai/Api.java
@@ -0,0 +1,41 @@
+package ch.heigvd.dai;
+
+import io.javalin.Javalin;
+
+public class Api {
+    private final Javalin app;
+    private final Sqlite database;
+
+    public Api(Sqlite database) {
+        this.app = Javalin.create();
+        this.database = database;
+    }
+
+    public void start(int port) {
+        app.get("/ping", ctx -> ctx.result("pong"));
+        // Partie user
+        app.get("/user", ctx -> ctx.result("list of user"));
+
+        app.get("/user/me", Main::getMe);
+
+        // A enlever peut être (get un user (nom, prenom, ...))
+        app.get("/user/{user_id}", ctx -> ctx.result("list of user items"));
+        app.patch("/user/me", ctx -> ctx.result("Update one or more param of my user"));
+        app.delete("/user/me", ctx -> ctx.result("To delete my account"));
+
+        app.post("/register", ctx -> ctx.result("Création du compte"));
+        app.post("/login", ctx -> ctx.result("Connextion"));
+
+        // Partie hdv
+        // Pour récuérer les offres d'un user avec /hdv?id=41
+        app.get("/hdv", ctx -> ctx.result("renvoi des objets"));
+        app.get("/hdv/{item_id}", ctx -> ctx.result("renvoi toutes les offres d'un objet"));
+
+        // Le sell_id est généré au moment du post (vente d'un objet)
+        app.delete("/hdv/sell/{sell_id}", ctx -> ctx.result("renvoi toutes les offres d'un objet"));
+        app.post("/hdv/sell", ctx -> ctx.result("Pour envoyer une offre"));
+        app.patch("/hdv/sell", ctx -> ctx.result("Pour modifier une offre (le prix de l'offre)"));
+
+        app.start(port);
+    }
+}
--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,6 +1,5 @@
 package ch.heigvd.dai;
 
-import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 import java.io.IOException;
@@ -22,33 +21,8 @@ public class Main {
         }
       }
 
-      Javalin app = Javalin.create();
-
-      app.get("/ping", ctx -> ctx.result("pong"));
-      // Partie user
-      app.get("/user", ctx -> ctx.result("list of user"));
-
-      app.get("/user/me", Main::getMe);
-
-      // A enlever peut être (get un user (nom, prenom, ...))
-      app.get("/user/{user_id}", ctx -> ctx.result("list of user items"));
-      app.patch("/user/me", ctx -> ctx.result("Update one or more param of my user"));
-      app.delete("/user/me", ctx -> ctx.result("To delete my account"));
-
-      app.post("/register", ctx -> ctx.result("Création du compte"));
-      app.post("/login", ctx -> ctx.result("Connextion"));
-
-      // Partie hdv
-      // Pour récuérer les offres d'un user avec /hdv?id=41
-      app.get("/hdv", ctx -> ctx.result("renvoi des objets"));
-      app.get("/hdv/{item_id}", ctx -> ctx.result("renvoi toutes les offres d'un objet"));
-
-      // Le sell_id est généré au moment du post (vente d'un objet)
-      app.delete("/hdv/sell/{sell_id}", ctx -> ctx.result("renvoi toutes les offres d'un objet"));
-      app.post("/hdv/sell", ctx -> ctx.result("Pour envoyer une offre"));
-      app.patch("/hdv/sell", ctx -> ctx.result("Pour modifier une offre (le prix de l'offre)"));
-
-      app.start(PORT);
+      Api api = new Api(database);
+      api.start(PORT);
     } catch (SQLException | IOException e) {
       System.err.println(e.getMessage());
     }
