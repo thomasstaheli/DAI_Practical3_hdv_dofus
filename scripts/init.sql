501f906a1bea9a34f3a1bc1ae14731f48641f60c
thomasstaheli <thomas_s@bluewin.ch>
2025-01-10 10:51:11 +0100

    Modify offer table and create inventory_user table

--- scripts/init.sql
+++ scripts/init.sql
@@ -15,6 +15,20 @@ CREATE TABLE offer (
     offer_id SERIAL PRIMARY KEY,
     item_id INT NOT NULL,
     user_id INT NOT NULL,
+    price_in_kamas INT NOT NULL,
+    quantity INT NOT NULL,
+
+    FOREIGN KEY (item_id) REFERENCES item(item_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE inventory_user (
+    user_id INT,
+    item_id INT,
+    quantity INT NOT NULL,
+
+    PRIMARY KEY (user_id, item_id),
+
     FOREIGN KEY (item_id) REFERENCES item(item_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
